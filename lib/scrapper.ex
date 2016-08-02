bdbea7e05d4eff143755916a7da6ff0f203359c8
amardaxini <amardaxini@gmail.com>
2016-08-01 22:11:04 -0700

    added feed data

--- lib/scrapper.ex
+++ lib/scrapper.ex
@@ -12,6 +12,14 @@ defmodule Diffbot.Router.Scrapper do
       json(conn,  Scrape.article(url))
        
     end
+    params do
+      requires :feed_url,  type: String
+    end
+    get "feed"  do
+      url = params[:feed_url] 
+      json(conn,  Scrape.feed(url))
+       
+    end
   end  
 
 end
